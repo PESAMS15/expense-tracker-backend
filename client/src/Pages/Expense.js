89d94dd6c830c336f35ffd850974aee15c549384
PESAMS15 <pesams015@gmail.com>
2024-01-06 13:33:26 +0100

    abeg work na#

--- client/src/Pages/Expense.js
+++ client/src/Pages/Expense.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 const Expense = async() => {
     const { id } = useParams()
+    console.log(id)
 
-    const res = await fetch(`https://expesne-tracker.onrender.com/expense/viewoneexpense/${id}`)
+    const res = await fetch(`https://expesne-tracker.onrender.com/expense/viewoneexpense/${id}`
+     )
     const data = await res.json();
     console.log(data)
 
